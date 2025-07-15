4b83da271cd9dad0463157089dff448046cb46fb
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-15 11:10:49 +0200

    Update PixelShader8.h

--- src/PixelShader8.h
+++ src/PixelShader8.h
@@ -84,6 +84,7 @@ private:
 	HRESULT Create_VertexBufferDynamic_D3D11(ID3D11Device* pDevice);
 	HRESULT Update_VertexBufferDynamic_D3D11(ID3D11DeviceContext* ctx);
 	HRESULT Update_Vertices_D3D11();
+	HRESULT GetInfoFromShaderResourceView(ID3D11ShaderResourceView* pShaderResourceView);
 
 	
 	ID3D11Device* pD3DDevice;
