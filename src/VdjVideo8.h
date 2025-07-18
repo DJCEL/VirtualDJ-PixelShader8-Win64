16ecc0a0bad9ffa6b11942ce7f5dc2219ec83eb0
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-18 23:53:56 +0200

    Update VdjVideo8.h

--- src/VdjVideo8.h
+++ src/VdjVideo8.h
@@ -64,11 +64,13 @@ struct IVdjVideoCallbacks8
 
 	//For DirectX 9 (windows 32-bit) device is IDirect3DDevice9*
 	//For DirectX 11 (windows 64-bit) device is ID3D11Device*
+ 	//For Metal (macOS) device is id<MTLRenderCommandEncoder>
 	virtual HRESULT GetDevice(EVdjVideoEngine engine, void **device)=0;
 
 	//For DirectX 9 (windows 32-bit) texture is IDirect3DTexture9*
 	//For DirectX 11 (windows 64-bit) texture is ID3D11ShaderResourceView*
 	//For OpenGL (macOS) texture is a regular opengl texture id, with type GLuint
+	//For Metal (macOS) texture is id<MTLTexture>
 	virtual HRESULT GetTexture(EVdjVideoEngine engine, void **texture, TVertex8 **vertices)=0;
 };
 
@@ -88,11 +90,19 @@ public:
 	HRESULT (*DrawDeck)(int deck, TVertex8* vertices);
 
 	// for more complicated operations, you can ask direct access to the device and textures
-	// For OpenGL texture needs to point to a GLuint, for DirectX 9, it needs to point to a IDirect3DTexture9*
+
+	//For DirectX 9 (windows 32-bit) device is IDirect3DDevice9*
+	//For DirectX 11 (windows 64-bit) device is ID3D11Device*
+        //For Metal (macOS) device is id<MTLRenderCommandEncoder>
 	HRESULT (*GetDevice)(EVdjVideoEngine engine, void **device);
+
+	//For DirectX 9 (windows 32-bit) texture is IDirect3DTexture9*
+	//For DirectX 11 (windows 64-bit) texture is ID3D11ShaderResourceView*
+	//For OpenGL (macOS) texture is a regular opengl texture id, with type GLuint
+	//For Metal (macOS) texture is id<MTLTexture>
 	HRESULT (*GetTexture)(EVdjVideoEngine engine, int deck, void**texture);
 
-	// When DirectX/OpenGL is initialized or closed, these functions will be called
+	// When DirectX/OpenGL/Metal is initialized or closed, these functions will be called
 	virtual HRESULT VDJ_API OnDeviceInit() {return S_OK;}
 	virtual HRESULT VDJ_API OnDeviceClose() {return S_OK;}
 
@@ -120,7 +130,16 @@ public:
 	HRESULT (*DrawDeck)(int deck, TVertex8* vertices);
 	
 	// for more complicated operations, you can ask direct access to the device and textures
+
+	//For DirectX 9 (windows 32-bit) device is IDirect3DDevice9*
+	//For DirectX 11 (windows 64-bit) device is ID3D11Device*
+        //For Metal (macOS) device is id<MTLRenderCommandEncoder>
 	HRESULT (*GetDevice)(EVdjVideoEngine engine, void **device);
+
+	//For DirectX 9 (windows 32-bit) texture is IDirect3DTexture9*
+	//For DirectX 11 (windows 64-bit) texture is ID3D11ShaderResourceView*
+	//For OpenGL (macOS) texture is a regular opengl texture id, with type GLuint
+	//For Metal (macOS) texture is id<MTLTexture>
 	HRESULT (*GetTexture)(EVdjVideoEngine engine, int deck, void**texture);
 
 	// When DirectX/OpenGL is initialized or closed, these functions will be called
@@ -153,18 +172,22 @@ public:
 	// or call GetTexture to get low-level access to the texture and its vertices.
 	virtual HRESULT VDJ_API OnDraw()=0;
 
+	// for more complicated operations, you can ask direct access to the device and textures:
+
 	//For DirectX 9 (windows 32-bit) device is IDirect3DDevice9*
 	//For DirectX 11 (windows 64-bit) device is ID3D11Device*
+        //For Metal (macOS) device is id<MTLRenderCommandEncoder>
 	HRESULT GetDevice(EVdjVideoEngine engine, void **device) {return vcb->GetDevice(engine,device);}
 
 	//For DirectX 9 (windows 32-bit) texture is IDirect3DTexture9*
 	//For DirectX 11 (windows 64-bit) texture is ID3D11ShaderResourceView*
 	//For OpenGL (macOS) texture is a regular opengl texture id, with type GLuint
+	//For Metal (macOS) texture is id<MTLTexture>
 	HRESULT GetTexture(EVdjVideoEngine engine, void **texture, TVertex8 **vertices) {return vcb->GetTexture(engine,texture,vertices);}
 
 	HRESULT DrawDeck() {return vcb->DrawDeck();}
 
-	// When DirectX/OpenGL is initialized or closed, these functions will be called
+	// When DirectX/OpenGL/Metal is initialized or closed, these functions will be called
 	virtual HRESULT VDJ_API OnDeviceInit() {return S_OK;}
 	virtual HRESULT VDJ_API OnDeviceClose() {return S_OK;}
 
