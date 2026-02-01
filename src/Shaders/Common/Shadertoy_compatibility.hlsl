9bb04199b264c7b132ceed84a30d0a615cccd127
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-02-01 21:45:23 +0100

    update

--- src/Shaders/Common/Shadertoy_compatibility.hlsl
+++ src/Shaders/Common/Shadertoy_compatibility.hlsl
@@ -4,6 +4,7 @@
 #define vec2 float2
 #define vec3 float3
 #define vec4 float4
+#define mat2 float2x2
 #define mix lerp
 #define fract frac
 vec3 iResolution = float3(float(iResolutionWidth), float(iResolutionHeight), 0.0f);
