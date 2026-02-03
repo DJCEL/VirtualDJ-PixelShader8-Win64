96a412127c87e2baa325b0f58cfd006fcc602f2f
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-02-03 21:05:04 +0100

    update

--- src/Shaders/Common/Shadertoy_compatibility.hlsl
+++ src/Shaders/Common/Shadertoy_compatibility.hlsl
@@ -1,3 +1,43 @@
+////////////////////////////////
+// File: Shadertoy.hlsl
+////////////////////////////////
+
+//--------------------------------------------------------------------------------------
+// Textures and Samplers
+//--------------------------------------------------------------------------------------
+Texture2D g_Texture2D : register(t0);
+SamplerState g_SamplerState : register(s0);
+//--------------------------------------------------------------------------------------
+// Constant Buffer
+//--------------------------------------------------------------------------------------
+cbuffer PS_CONSTANTBUFFER : register(b0)
+{
+    int g_FX_ResolutionWidth;
+    int g_FX_ResolutionHeight;
+    float iTime;
+    bool g_FX_params_on;
+    float g_FX_param1;
+    float g_FX_param2;
+    float g_FX_param3;
+    float g_FX_param4;
+    float g_FX_param5;
+};
+//--------------------------------------------------------------------------------------
+// Input structure
+//--------------------------------------------------------------------------------------
+struct PS_INPUT
+{
+    float4 Position : SV_Position;
+    float4 Color : COLOR0;
+    float2 TexCoord : TEXCOORD0;
+};
+//--------------------------------------------------------------------------------------
+// Output structure
+//--------------------------------------------------------------------------------------
+struct PS_OUTPUT
+{
+    float4 Color : SV_TARGET;
+};
 //--------------------------------------
 // Shadertoy compatibility
 //--------------------------------------
@@ -8,8 +48,36 @@
 #define mix lerp
 #define fract frac
 #define mod fmod
-vec3 iResolution = float3(float(iResolutionWidth), float(iResolutionHeight), 0.0f);
-#define fragCoord (input.TexCoord * iResolution.xy)
-#define fragColor (PS_OUTPUT output; output.Color)
 #define iChannel0 g_SamplerState
 #define texture g_Texture2D.Sample
+vec3 iResolution = float3(float(g_FX_ResolutionWidth), float(g_FX_ResolutionHeight), 0.0f);
+//--------------------------------------------------------------------------------------
+// Shadertoy - mainImage()
+//--------------------------------------------------------------------------------------
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord / iResolution.xy;
+    vec3 col = texture(iChannel0, uv).xyz;
+    
+    col.g = col.r;
+    col.b = col.r;
+    
+    fragColor = vec4(col, 1.0f);
+}
+//--------------------------------------------------------------------------------------
+// Pixel Shader
+//--------------------------------------------------------------------------------------
+PS_OUTPUT ps_main(PS_INPUT input)
+{
+    float2 texcoord = input.TexCoord;
+    float2 fragCoord = texcoord * iResolution.xy;
+    float4 fragColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    mainImage(fragColor, fragCoord);
+    PS_OUTPUT output;
+    output.Color = fragColor;
+    output.Color = output.Color * input.Color;
+    return output;
+}
+
+
+
