097d0db1273e0e090896355cdb69258cb0d2df0d
djcel <16272026+DJCEL@users.noreply.github.com>
2026-02-03 07:49:11 +0100

    Update GrayScale.hlsl

--- src/Shaders/GrayScale.hlsl
+++ src/Shaders/GrayScale.hlsl
@@ -37,14 +37,15 @@ struct PS_OUTPUT
 //--------------------------------------------------------------------------------------
 PS_OUTPUT ps_main(PS_INPUT input)
 {
-    PS_OUTPUT output;
     float2 texcoord = input.TexCoord;
     float4 color = g_Texture2D.Sample(g_SamplerState, texcoord);
     
     color.gb = color.r;
 
+    PS_OUTPUT output;
     output.Color = color;
     output.Color = output.Color * input.Color;
     
     return output;
 }
+
