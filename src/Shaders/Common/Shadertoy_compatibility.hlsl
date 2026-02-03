8d02b94a2153c47ef34129f55e1e627299fdd8b1
djcel <16272026+DJCEL@users.noreply.github.com>
2026-02-03 07:03:43 +0100

    Update Shadertoy_compatibility.hlsl

--- src/Shaders/Common/Shadertoy_compatibility.hlsl
+++ src/Shaders/Common/Shadertoy_compatibility.hlsl
@@ -9,7 +9,7 @@
 #define fract frac
 #define mod fmod
 vec3 iResolution = float3(float(iResolutionWidth), float(iResolutionHeight), 0.0f);
-#define fragCoord input.TexCoord * iResolution.xy
-#define fragColor PS_OUTPUT output; output.Color
+#define fragCoord (input.TexCoord * iResolution.xy)
+#define fragColor (PS_OUTPUT output; output.Color)
 #define iChannel0 g_SamplerState
 #define texture g_Texture2D.Sample
