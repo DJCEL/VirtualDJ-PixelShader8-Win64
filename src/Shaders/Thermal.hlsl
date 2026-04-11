dd56104e14b06c15e77f64a4d3a794b41cd348e4
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-04-11 16:45:01 +0200

    update

--- src/Shaders/Thermal.hlsl
+++ src/Shaders/Thermal.hlsl
@@ -43,13 +43,7 @@ struct PS_OUTPUT
 //--------------------------------------------------------------------------------------
 // Additional functions
 //--------------------------------------------------------------------------------------
-float3 thermal(float3 color)
-{
-    float3 invColor = float3(1.0f, 1.0f, 1.0f) - color;
-    float len = pow((length(invColor * 2.2f)) / 3.0f, 2.0f);
-    float3 col = float3(len, len * pow((1.0f - color.r), 2.0f), 0.0f);
-    return float3(len * 1.5f, len * pow((1.0f - color.r), 2.0f), 0.0f) + dot(col, float3(0.0f, 1.0f, 0.0f)) / 1.5f;
-}
+
 //--------------------------------------------------------------------------------------
 // Pixel Shader
 //--------------------------------------------------------------------------------------
@@ -58,7 +52,12 @@ PS_OUTPUT ps_main(PS_INPUT input)
     float2 texcoord = input.TexCoord;
     float3 inColor = g_Texture2D.Sample(g_SamplerState, texcoord).rgb;
     
-    float4 color = float4(thermal(inColor), 1.0f);
+    float3 invColor = 1.0f - inColor;
+    float len = pow((length(invColor * 2.2f)) / 3.0f, 2.0f);
+    float3 col = float3(len, len * pow((1.0f - inColor.r), 2.0f), 0.0f);
+    float3 thermalColor= float3(len * 1.5f, len * pow((1.0f - inColor.r), 2.0f), 0.0f) + dot(col, float3(0.0f, 1.0f, 0.0f)) / 1.5f;
+    
+    float4 color = float4(thermalColor, 1.0f);
     
     PS_OUTPUT output;
     output.Color = color;
