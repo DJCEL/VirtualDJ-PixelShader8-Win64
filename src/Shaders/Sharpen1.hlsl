812801d78d31e0cfef583f8dfd54d121f1e99e1a
DJCEL <16272026+DJCEL@users.noreply.github.com>
2026-07-26 20:32:51 +0200

    update

--- src/Shaders/Sharpen1.hlsl
+++ src/Shaders/Sharpen1.hlsl
@@ -85,15 +85,15 @@ PS_OUTPUT ps_main(PS_INPUT input)
     // Sharpen kernel weights
     float kernel[KERNEL_SIZE] =
     {
-        -1.0, // top-left
-        -1.0, // top-center
-        -1.0, // top-right
-        -1.0, // center-left
-        8.0 + Amount, // center
-        -1.0, // center-right
-        -1.0, // bottom-left
-        -1.0, // bottom-center
-        -1.0 // bottom-right
+        -1.0f, // top-left
+        -1.0f, // top-center
+        -1.0f, // top-right
+        -1.0f, // center-left
+        8.0f + Amount, // center
+        -1.0f, // center-right
+        -1.0f, // bottom-left
+        -1.0f, // bottom-center
+        -1.0f // bottom-right
     };
 
     float3 texcolor = float3(0.0f, 0.0f, 0.0f);
